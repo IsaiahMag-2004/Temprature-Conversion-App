a8d8e9e54b6dc3c626b0868561367d919d1fed1e
Isaiah Magana <Maganaisaiah4@gmail.com>
2021-12-22 12:52:20 -0500

    Completed main project

---
+++ Main.py
@@ -0,0 +1,13 @@
+#unit 1 challanger 3 Temprature Conversion App
+print("Welcome to the Temprature Conversion App/n")
+
+#Get Imperial Temprature
+fahrenheit = float(input("What is the given temprature in degrees fahrenheit: "))
+
+#Convert and output
+celcius = round((fahrenheit - 32) * 5.0 / 9), 2)
+kelvin = round((fahrenheit - 32) * 5.0 / 9 + 273.15, 2))
+
+print(f"Degrees Fahrenheit:    {fahrenheit}")
+print(f"Degrees Celcius:       {celcius}")
+print(f"Degrees Kelvin:        {kelvin}")
